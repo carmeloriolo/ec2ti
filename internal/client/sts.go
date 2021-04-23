0ad2ffef1a577b28fe5af89adbb3a0b666d53b18
carmeloriolo <carmeloriolo93@gmail.com>
2021-04-23 21:34:58 +0200

    feat: updated sts client, also added tests

--- internal/client/sts.go
+++ internal/client/sts.go
@@ -7,21 +7,35 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
-type Sts struct {
-	client *sts.Client
+type CallerIdentity struct {
+	userId  string
+	account string
+	arn     string
 }
 
-func NewSts(cfg aws.Config) *Sts {
-	s := Sts{
+type StsClientInterface interface {
+	GetCallerIdentity(ctx context.Context, params *sts.GetCallerIdentityInput, optFns ...func(*sts.Options)) (*sts.GetCallerIdentityOutput, error)
+}
+
+type StsClient struct {
+	client StsClientInterface
+}
+
+func NewSts(cfg aws.Config) *StsClient {
+	s := StsClient{
 		client: sts.NewFromConfig(cfg),
 	}
 	return &s
 }
 
-func (s *Sts) GetCallerArn() (string, error) {
+func (s *StsClient) GetCallerIdentity() (*CallerIdentity, error) {
 	identity, err := s.client.GetCallerIdentity(context.Background(), &sts.GetCallerIdentityInput{})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return *identity.Arn, nil
+	return &CallerIdentity{
+		userId:  *identity.UserId,
+		account: *identity.Account,
+		arn:     *identity.Arn,
+	}, nil
 }
---
+++ internal/client/sts_test.go
@@ -0,0 +1,86 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+const (
+	callerIdentity = "callerIdentity"
+	errorIdentity  = "errorIdentity"
+)
+
+type StsClientMock struct {
+	testCase string
+}
+
+func (s *StsClientMock) GetCallerIdentity(ctx context.Context, params *sts.GetCallerIdentityInput, optFns ...func(*sts.Options)) (*sts.GetCallerIdentityOutput, error) {
+	switch s.testCase {
+	case callerIdentity:
+		stsCallerIdentityOutput := sts.GetCallerIdentityOutput{}
+		stub, err := ioutil.ReadFile("./stubs/get-caller-identity.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		json.Unmarshal([]byte(string(stub)), &stsCallerIdentityOutput)
+		return &stsCallerIdentityOutput, nil
+	default:
+		return nil, errors.New("error")
+	}
+}
+
+func getStsMock(s string) *StsClient {
+	return &StsClient{
+		client: &StsClientMock{testCase: s},
+	}
+}
+
+func Test_GetCallerIdentity(t *testing.T) {
+
+	tests := []struct {
+		name         string
+		sts          *StsClient
+		wantIdentity *CallerIdentity
+		wantError    error
+	}{
+		{
+			name:         "Testing error case",
+			sts:          getStsMock(errorIdentity),
+			wantIdentity: nil,
+			wantError:    errors.New("error"),
+		},
+		{
+			name: "Testing get caller identity",
+			sts:  getStsMock(callerIdentity),
+			wantIdentity: &CallerIdentity{
+				userId:  "AKIDSJHUFAHUDASMK",
+				account: "48120983120391",
+				arn:     "arn:aws:iam::48120983120391:user/user.mock",
+			},
+			wantError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		st := tt
+		t.Run(tt.name, func(t *testing.T) {
+			identity, err := st.sts.GetCallerIdentity()
+			if (st.wantError != nil && err == nil) || (st.wantError == nil && err != nil) {
+				t.Errorf("GetCallerIdentity() st.wantError = %s err = %s", st.wantError, err)
+				return
+			}
+			if !reflect.DeepEqual(st.wantIdentity, identity) {
+				t.Errorf("GetCallerIdentity() st.wantIdentity = %s identity = %s", st.wantError, err)
+				return
+			}
+		})
+	}
+
+}
