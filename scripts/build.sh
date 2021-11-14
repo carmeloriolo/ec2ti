9f362273e7c37dc3329a2dfda085f2b718b8032a
carmeloriolo <carmeloriolo93@gmail.com>
2021-11-14 01:46:20 +0100

    ci: updated workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,16 +9,14 @@ PLATFORMS="darwin/arm64 darwin/amd64"
 
 
 for PLATFORM in $PLATFORMS; do
-  mkdir -p built
   GOOS=${PLATFORM%/*}
   GOARCH=${PLATFORM#*/}
   APP="ec2ti"
   echo "Building ${PLATFORM}"
   if [[ "${GOOS}" == "windows" ]]; then APP="ec2ti.exe"; fi
-  GOOS=${GOOS} GOARCH=${GOARCH} go build -v -o ./bin/${APP} -ldflags="-X github.com/carmeloriolo/ec2ti/cmd/main.AppVersion=${VERSION}" ./cmd/main.go
+  GOOS=${GOOS} GOARCH=${GOARCH} go build -v -o ./bin/${APP} -ldflags="-X main.AppVersion=${VERSION}" ./cmd/main.go
   cd ./bin
   tar cvzf ${GOOS}-${GOARCH}-ec2ti.tar.gz ${APP}
-  mv ${GOOS}-${GOARCH}-ec2ti.tar.gz ../built/
-  cd -
+  cd .. 
 done
 
